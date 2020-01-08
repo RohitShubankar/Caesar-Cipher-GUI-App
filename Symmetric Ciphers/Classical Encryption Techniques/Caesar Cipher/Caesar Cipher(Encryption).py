a05cd22adce666da301f1e7685062a718335de99
Shubankar <rohit.shubankar@gmail.com>
2020-01-08 20:56:43 +0530

    Update Caesar Cipher(Encryption).py

--- Symmetric Ciphers/Classical Encryption Techniques/Caesar Cipher/Caesar Cipher(Encryption).py
+++ Symmetric Ciphers/Classical Encryption Techniques/Caesar Cipher/Caesar Cipher(Encryption).py
@@ -1,9 +1,10 @@
-''' Caesar Cipher: One of the earliest and the simplest encryption technique.
+'''
+    Caesar Cipher: One of the earliest and the simplest encryption technique.
     The Caesar Cipher involves replacing the each letter in the given plain text
     with the letter standing three places further down the Alphabet.
 
-    Example : Plain Text -  Hello World.
-              Cipher Text - Khoor zruog
+    Example : Plain Text -  HelloWorld
+              Cipher Text - KhoorZruog
     Encryption Algorithm : C = E(K,P) = (P+K)mod26
     where C: Cipher text
           K: Key = 3
@@ -18,7 +19,7 @@ def CaesarCipher_Encryption(plain_text,key):
         else:
             cipher_text+=chr((ord(letter)+key-97)%26+97)
     return cipher_text
-plain_text = input("Enter the Plain Text:")
+plain_text = input("Enter the Plain Text without any space:")
 key = 3
 print("The Plain Text is:",plain_text)
 print("The Cipher Text is:",CaesarCipher_Encryption(plain_text,key))
